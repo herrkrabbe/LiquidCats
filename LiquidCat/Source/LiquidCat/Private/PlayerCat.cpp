c28e0d4df237c6607ef5774a10f46878065570d3
TommyB-DontMindMe <142879081+TommyB-DontMindMe@users.noreply.github.com>
2024-01-27 13:30:13 +0100

    Continued Work on movement implementation

--- LiquidCat/Source/LiquidCat/Private/PlayerCat.cpp
+++ LiquidCat/Source/LiquidCat/Private/PlayerCat.cpp
@@ -5,7 +5,61 @@
 
 APlayerCat::APlayerCat()
 {
+	PrimaryActorTick.bCanEverTick = true;
+
+	PlayerSprite = CreateDefaultSubobject<UPaperFlipbookComponent>(TEXT("PlayerSprite"));
+	SetRootComponent(PlayerSprite);
+}
+
+void APlayerCat::BeginPlay()
+{
+	Super::BeginPlay();
+	InitLocation = PlayerSprite->GetComponentLocation();
+
+	APlayerController* PlayerController = Cast<APlayerController>(Controller);
+	if (PlayerController) {
+		UEnhancedInputLocalPlayerSubsystem* subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer());
+		if (subsystem) {
+			subsystem->AddMappingContext(IMCPlayerInput, 0);
+
+		}
+	}
+}
+
+void APlayerCat::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	if (Speed) {
+		PlayerSprite->AddRelativeLocation(FVector(DeltaX, DeltaY, 0.f) * Speed);
+	}
 
 }
 
+void APlayerCat::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	if (UEnhancedInputComponent* EnhanceInputCom = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhanceInputCom->BindAction(IA_MoveRigthLeft, ETriggerEvent::Triggered, this, &APlayerCat::MoveXAxis);
+		EnhanceInputCom->BindAction(IA_MoveRigthLeft, ETriggerEvent::Completed, this, &APlayerCat::MoveXAxisCompleted);
+	}
+}
 
+
+void APlayerCat::MoveXAxis(const FInputActionValue& val)
+{
+	const float getVal = val.Get<float>();
+	if (getVal)
+	{
+		DeltaX = getVal;
+	}
+}
+
+void APlayerCat::MoveXAxisCompleted(const FInputActionValue& val)
+{
+	const float getVal = val.Get<float>();
+	if (getVal)
+	{
+		Speed = 0;
+	}
+}
